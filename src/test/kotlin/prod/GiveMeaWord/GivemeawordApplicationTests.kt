f3ca79f5788495f96cc9d1c476208b6669cc8b2e
alex.u <alex.u@99devs.ru>
2019-12-02 13:26:57 +0200

    fix test

--- src/test/kotlin/prod/GiveMeaWord/GivemeawordApplicationTests.kt
+++ src/test/kotlin/prod/GiveMeaWord/GivemeawordApplicationTests.kt
@@ -2,6 +2,7 @@ package prod.GiveMeaWord
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import prod.GiveMeaWord.Service.TelegramService
@@ -9,10 +10,13 @@ import java.net.URL
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class GivemeawordApplicationTests  {
+class GivemeawordApplicationTests {
+
+    @Autowired
+    lateinit var telegramService: TelegramService
 
     @Test
-    fun printTest(){
+    fun printTest() {
         print("Test")
     }
 
@@ -24,8 +28,8 @@ class GivemeawordApplicationTests  {
 
     //Check sending function to Telegram bot
     @Test
-    fun sendToUserThroughKOHttpToTelegramBot(){
-        val result = TelegramService().sendText("Doing a test")
+    fun sendToUserThroughKOHttpToTelegramBot() {
+        val result = telegramService.sendText("Doing a test")
         assert(result.isSuccessful)
     }
 }
